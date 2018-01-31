bd7fa5c119f31ccb8738988603990b0265efd062
Viraj <viraj@Vishnus-iMac.local>
2018-01-31 19:31:52 +0530

    first commit

---
+++ src/com/viraj/Comparisons/Comparison.java
@@ -0,0 +1,33 @@
+package com.viraj.Comparisons;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean isYoung = false;
+		System.out.println(isYoung);
+		
+		int age = 25;
+		System.out.println(age);
+		
+		isYoung = age < 50;
+		System.out.println(isYoung);
+		
+		System.out.println("\n--------------- NEW DATA --------------\n");
+		
+		boolean isPowerful = false;
+		System.out.println("The value of isPowerful variable is: " + isPowerful);
+		
+		int energy = 57;
+		System.out.println("The value of the energy is: " + energy);
+		
+		isPowerful = energy > 80;
+		System.out.println("The value of isPowerful evaluates to: " + isPowerful);
+		
+		
+		
+
+	}
+
+}
